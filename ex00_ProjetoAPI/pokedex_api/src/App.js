7864ec4a017fafc577021775b7ba205bfb599eff
Allison Rodrigues <allison_rps@hotmail.com>
2024-03-19 21:01:52 -0300

    Projeto Pokedex

--- ex00_ProjetoAPI/pokedex_api/src/App.js
+++ ex00_ProjetoAPI/pokedex_api/src/App.js
@@ -1,32 +1,72 @@
-import React, { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import './style.css';
 
 function App() {
-  const {pokemon, setPokemon} = useState();
+  const [pokemon, setPokemon] = useState({});
+  const [pokemonName, setPokemonName] = useState("");
 
-function loadAPI(){
-  let url = 'https://www.pokeapi.co/api/v2/pokemon/giratina'
-  fetch(url)
-  .then(response => response.json())
-  .then(json => {
-    console.log(json)
-    setPokemon(json)
-  })
-  .catch(err => console.log(err));
-}
+  function loadAPI(name){
+    let url = `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(res => {
+        console.log(res);
+        setPokemon(res);
+      })
+      .catch(err => console.log(err));
+  }
 
-  useEffect (() => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    loadAPI(pokemonName);
+  }
 
-    loadAPI();
-
+  useEffect(() => {
+    // Carrega o Pokémon Ditto por padrão
+    loadAPI("ditto");
   }, []);
 
   return (
-
-    <div class="container">
+    <div className="container">
       <header>
         <strong>POKÉMON API</strong>
       </header>
+      
+      <form className="search"onSubmit={handleSubmit}>
+
+         <br></br>
+
+        <input 
+          type="text" 
+          value={pokemonName} 
+          onChange={(e) => setPokemonName(e.target.value)} 
+          placeholder="Pesquisar" 
+          required 
+        />
+
+        <button type="submit">Pesquisar</button>
+      </form>
+
+      <div className="pokemon-info">
+        <div className="pokemon-number">
+          <div># {pokemon.id}</div>
+        </div>
+        <div className="pokemon-name">
+
+          <div>{pokemon.name}</div>
+        </div>
+      </div>
+      <div className="pokemon-details">
+        <div className="pokemon-image">
+          <img src={pokemon.sprites && pokemon.sprites.front_default} alt={pokemon.name} />
+        </div>
+        <div className="pokemon-stats">
+          <div>PESO: {pokemon.weight / 10} KG.</div>
+          <div>ALTURA: {pokemon.height / 10} M.</div>
+          <div>TIPO: {pokemon.types && pokemon.types.map(type => type.type.name).join(', ')}</div>
+          <div>HABILIDADES: {pokemon.ability}</div>
+        </div>
+      </div>
     </div>
   );
 }
